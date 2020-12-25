4d922b571fef4a5d86150b8767951a0e2049b024
David Brooks <ms@davebeta.com>
2020-12-25 15:35:32 +0000

    Team turn order is now enforced

--- Models/GameModel.cs
+++ Models/GameModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace colander_game.Models
@@ -22,6 +23,54 @@ namespace colander_game.Models
         }
 
         public int RoundNumber { get; set; }
+
+        public int LastTeamToPlay { get; set; }
+
+        public void EndPlayersGo(bool endOfRound = false)
+        {
+            ActivePlayer = null;
+            ActivePaper = null;
+            if (!endOfRound)
+            {
+                // Move play to the next team
+                LastTeamToPlay++;
+                if (LastTeamToPlay >= Teams.Count)
+                {
+                    LastTeamToPlay = 0;
+                }
+            }
+        }
+
+        public string WhichTeamIsUp()
+        {
+            if (Teams == null)
+            {
+                return "";
+            }
+
+            var nextTeamNum = LastTeamToPlay + 1;
+            Team team = null;
+            int goneRound = 0;
+            while (goneRound < 2)
+            {
+                if (Teams.Count <= nextTeamNum)
+                {
+                    goneRound++;
+                    nextTeamNum = 0;
+                    continue;
+                }
+
+                team = Teams[nextTeamNum];
+                if (team.Players == null || team.Players.Count == 0)
+                {
+                    nextTeamNum++;
+                    continue;
+                }
+                break;
+            }
+
+            return team.Name;
+        }
 
         [JsonPropertyName("id")]
         public string GameId { get; set; }
@@ -55,6 +104,11 @@ namespace colander_game.Models
                 return false;
             }
             return true;
+        }
+
+        public Team CurrentTeam(string userId)
+        {
+            return Teams?.FirstOrDefault(t => t.Players != null && t.Players.Any(p => p.UserId == userId));
         }
     }
 }
