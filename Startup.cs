af7dc90351950cadb4056ed6cb655fc01b9a0e81
David Brooks <ms@davebeta.com>
2020-12-25 01:57:39 +0000

    small change

--- Startup.cs
+++ Startup.cs
@@ -51,6 +51,7 @@ namespace colander_game
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
