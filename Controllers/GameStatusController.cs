f528b22bf149d00250f4498a4c43ac140e788cdd
David Brooks <ms@davebeta.com>
2021-01-24 21:17:12 +0000

    Split out into partial views and created first attempt at a status endpoint for dynamic page updates

---
+++ Controllers/GameStatusController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using colander_game.Models;
+using colander_game.Services;
+
+namespace colander_game.Controllers
+{
+    public class GameStatusController : Controller
+    {
+        private ISessionService _sessionService;
+        private IGameStateService _gameService;
+
+        public GameStatusController(ISessionService sessionService, IGameStateService gameService)
+        {
+            _sessionService = sessionService;
+            _gameService = gameService;
+        }
+
+        [Route("status/{gameId}")]
+        public async Task<IActionResult> Index(string gameId)
+        {
+            gameId = gameId.FormatGameId();
+
+            var userId = _sessionService.GetUserId(Request, Response);
+            var user = await _sessionService.GetUserData(userId);
+            var game = await _gameService.GetGameAsync(gameId, userId);
+
+            return View(new GameRenderModel
+            {
+                User = user ?? new UserModel(),
+                Game = game
+            });
+        }
+    }
+}
