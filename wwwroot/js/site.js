8e011c50256f1609e8af58536b2f74f6cee1c864
David Brooks <ms@davebeta.com>
2021-01-29 15:45:14 +0000

    ReadyToStart partial view created and page reloads removed from round 0

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,20 +10,32 @@ function loadStatusHtml(url)
         // Convert the HTML string into a document object
         var parser = new DOMParser();
         var doc = parser.parseFromString(html, 'text/html');
+        
+        if (doc.documentElement.dataset.round != "0")
+        {
+            // Game has started - reload the page to move on
+            window.location.reload();
+        }
 
-        var statusBox = document.getElementById("colander-status-box");
+        var statusBox = document.getElementById('colander-status-box');
         var statusData = doc.querySelector('div#colander-status-box');
         statusBox.innerHTML = statusData.innerHTML;
 
-        var teamsBox = document.getElementById("teams-box");
+        var teamsBox = document.getElementById('teams-box');
         var teamsData = doc.querySelector('div#teams-box');
         teamsBox.innerHTML = teamsData.innerHTML;
+
+        var startBox = document.getElementById('game-start-box');
+        if (startBox != null)
+        {
+            var startData = doc.querySelector('div#game-start-box');
+            startBox.innerHTML = startData.innerHTML;
+        }
 
         spinner.style.display = "none";
 
     }).catch(function (err) {
-        // There was an error
-        console.warn('Something went wrong.', err);
+        console.warn('Ooops', err);
     });
 } 
 
@@ -33,11 +45,7 @@ var gameId = gameArea.dataset.gameId;
 
 if (round == "0")
 {
-    var refreshTimer = setInterval(function(){
-        //if (timeleft <= 0) {
-        //    clearInterval(refreshTimer);
-        //    setTimeout(function(){ location.reload(); }, 10500);
-        //}
-        loadStatusHtml("/status/" + gameId);
+    var refreshTimer = setInterval(function() {
+        loadStatusHtml("/data/pregame/" + gameId);
     }, 5000);
 }
